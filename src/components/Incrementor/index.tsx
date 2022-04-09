64a1ef88b861a43997137659c934e77c99eacd2a
Laysa <laysa.viana@outlook.com>
2022-04-09 20:20:57 -0300

    funcionalidade de incrementar e decrementar

--- src/components/Incrementor/index.tsx
+++ src/components/Incrementor/index.tsx
@@ -1,25 +1,45 @@
 import { Plus as PlusIcon } from "@styled-icons/boxicons-regular/Plus";
 import { Subtract as SubtractIcon } from "@styled-icons/remix-fill/Subtract";
+import { useState } from "react";
 
 import { Wrapper, IconWrapper, Quantity } from "./styles";
 
 type IncrementorProps = {
   id: number;
   quantity: number;
+  quantidadeEstoque: number;
 };
 
-const Incrementor = ({ id, quantity }: IncrementorProps) => (
-  <Wrapper>
-    <IconWrapper>
-      <SubtractIcon aria-label="Subtract item" />
-    </IconWrapper>
+const Incrementor = ({ id, quantity, quantidadeEstoque }: IncrementorProps) => {
+  const [quantidade, setQuantidade] = useState(1);
 
-    <Quantity>{quantity}</Quantity>
+  const validarQuantidade = (tipoDaOperacao: string) => {
 
-    <IconWrapper>
-      <PlusIcon aria-label="Add item" />
-    </IconWrapper>
-  </Wrapper>
-);
+    if (tipoDaOperacao === "subtrair") {
+      if (quantidade > 0) {
+        setQuantidade(quantidade - 1);
+      }
+    }
+    else if (tipoDaOperacao === "adicionar") {
+      if (quantidadeEstoque >= quantidade) {
+        setQuantidade(quantidade + 1);
+      }
+    }
+  };
+
+  return (
+    <Wrapper>
+      <IconWrapper onClick={() => validarQuantidade("subtrair")}>
+        <SubtractIcon aria-label="Subtract item" />
+      </IconWrapper>
+
+      <Quantity>{quantidade}</Quantity>
+
+      <IconWrapper onClick={() => validarQuantidade("adicionar")}>
+        <PlusIcon aria-label="Add item" />
+      </IconWrapper>
+    </Wrapper>
+  );
+};
 
 export default Incrementor;
