fa1d0526af6a477721eb3bc1be29bde93ea1f431
Laysa <laysa.viana@outlook.com>
2022-04-09 22:09:51 -0300

    Erro no context

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/global";
 import { theme } from "./styles/theme";
 
+//import CartProvider from './src/contexts/CartContext';
+
 import Home from "./pages/home";
 
 const App = () => {
   return (
+    // <CartProvider>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Home />
     </ThemeProvider>
+    // </CartProvider>
   );
 };
 
